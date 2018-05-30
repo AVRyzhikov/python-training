a1b4086b050dcb6b399611c7809017e0b209e88b
avryzhikov <navigatort@yandex.ru>
2018-05-30 18:37:30 +0300

    Улучшены тесты.

--- test_add_group.py
+++ test_add_group.py
@@ -18,20 +18,20 @@ class test_add_group(unittest.TestCase):
     
     def test_add_group(self):
         driver = self.driver
-        self.open_home_page(driver)
+
         self.login(driver, username="admin", password="secret")
-        self.open_groups_page(driver)
+
         self.create_group(driver, Group(name="xxx",header="yyyy",footer="zzzzz"))
-        self.return_to_groups_page(driver)
+
         self.logout(driver)
 
     def test_add_empty_group(self):
         driver = self.driver
-        self.open_home_page(driver)
+
         self.login(driver, username="admin", password="secret")
-        self.open_groups_page(driver)
+
         self.create_group(driver, Group(name="",header="",footer=""))
-        self.return_to_groups_page(driver)
+
         self.logout(driver)
     def logout(self, driver):
         driver.find_element_by_link_text("Logout").click()
@@ -40,6 +40,7 @@ class test_add_group(unittest.TestCase):
         driver.find_element_by_link_text("group page").click()
 
     def create_group(self, driver, group):
+        self.open_groups_page(driver)
         #init group creation
         driver.find_element_by_xpath("(//input[@name='new'])[2]").click()
         # fill group form
@@ -54,11 +55,14 @@ class test_add_group(unittest.TestCase):
         driver.find_element_by_name("group_footer").send_keys(group.footer)
         # submit group creation
         driver.find_element_by_name("submit").click()
+        self.return_to_groups_page(driver)
 
     def open_groups_page(self, driver):
         driver.find_element_by_link_text("groups").click()
 
     def login(self, driver, username, password):
+        self.open_home_page(driver)
+        driver.get("http://localhost/addressbook/")
         driver.find_element_by_name("user").click()
         driver.find_element_by_name("user").clear()
         driver.find_element_by_name("user").send_keys(username)
