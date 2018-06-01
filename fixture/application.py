aa9c5bf4b11a16dd171d9db2c628cb1d970ee7c9
avryzhikov <navigatort@yandex.ru>
2018-06-01 17:31:34 +0300

    Выделен класс для работы с группами

--- fixture/application.py
+++ fixture/application.py
@@ -1,13 +1,17 @@
 #from selenium.webdriver.firefox.webdriver import WebDriver
 from selenium import webdriver
 from fixture.session import SessionHelper
+from fixture.group import GroupHelper
+
 class Application:
 
     def __init__(self):
         self.driver = webdriver.Firefox()
         self.driver.implicitly_wait(60)
+
         self.session=SessionHelper(self) # помощник получает ссылку на объект
                                          # класса Application
+        self.group = GroupHelper(self)
 
 
 
@@ -16,32 +20,7 @@ class Application:
         driver.get('http://localhost/addressbook')
 
 
-    def return_to_groups_page(self):
-        driver = self.driver
-        driver.find_element_by_link_text("group page").click()
 
-    def open_groups_page(self):
-        driver = self.driver
-        driver.find_element_by_link_text("groups").click()
-
-    def create_group(self, group):
-        driver = self.driver
-        self.open_groups_page()
-        # init group creation
-        driver.find_element_by_xpath("(//input[@name='new'])[2]").click()
-        # fill group form
-        driver.find_element_by_name("group_name").click()
-        driver.find_element_by_name("group_name").clear()
-        driver.find_element_by_name("group_name").send_keys(group.name)
-        driver.find_element_by_name("group_header").click()
-        driver.find_element_by_name("group_header").clear()
-        driver.find_element_by_name("group_header").send_keys(group.header)
-        driver.find_element_by_name("group_footer").click()
-        driver.find_element_by_name("group_footer").clear()
-        driver.find_element_by_name("group_footer").send_keys(group.footer)
-        # submit group creation
-        driver.find_element_by_name("submit").click()
-        self.return_to_groups_page()
 
     def destroy(self):
         self.driver.quit()
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -12,12 +12,12 @@ def app(request, version='1.0'):
 def test_add_group(app):
 
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="xxx",header="yyyy",footer="zzzzz"))
+    app.group.create(Group(name="xxx", header="yyyy", footer="zzzzz"))
     app.session.logout()
 
 def test_add_empty_group(app):
 
     app.session.login( username="admin", password="secret")
-    app.create_group(Group(name="",header="",footer=""))
+    app.group.create(Group(name="", header="", footer=""))
     app.session.logout()
 
